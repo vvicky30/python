057c84d3a25059a9e1fa49dc78038094ba606a03
Cloud User <ec2-user@ip-172-31-8-172.ap-south-1.compute.internal>
2019-06-18 19:49:55 +0000

    illustration of dynamic function & clear-utility function

---
+++ fun2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+#here we making clear-function ;as python doesn't support clear function:-
+def clear():
+  print('\n'*10)    #here we using 10 times-new-line approach
+
+
+clear()#calling of clear function
+
+#now we making dynamic function
+def add_mul(x,y):
+   print(x+y)
+   print(x*y)
+
+#callinf of function add_mul
+add_mul(2,4)  #calling with value-arguments
+
