ff15ea84717ad5a1a9de3fe0832c97802ec6a16d
Cloud User <ec2-user@ip-172-31-8-172.ap-south-1.compute.internal>
2019-06-18 19:11:23 +0000

    creating several static function

---
+++ fun.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+#creating a function hello
+def hello():
+ print("hello i am python")
+ print(55+5)
+hello()  #calling function
+
+#creating function hello
+def a():
+ print("calling function hello through a-function")
+ hello()
+a()
+
