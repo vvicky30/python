ea52009042ab1284b134f7a8a80ba059c44dbe15
Cloud User <ec2-user@ip-172-31-8-172.ap-south-1.compute.internal>
2019-06-18 21:17:38 +0000

    here we made a function which takes input from command inline

---
+++ fun_sys.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+#here we make command inline function
+
+
+import sys
+num1=sys.argv[1]#this will print inline input
+num2=sys.argv[2]
+print(num1)
+print(num2)
+
+
+#definition of function
+def add_mul(x,y):
+    print(x+y)
+    print(x*y)
+
+
+#call
+add_mul(int(num1),int(num2))
