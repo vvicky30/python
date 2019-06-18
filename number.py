935342c58ef4343ec99a3671130ba94d603b1027
Cloud User <ec2-user@ip-172-31-8-172.ap-south-1.compute.internal>
2019-06-18 18:43:15 +0000

    illustrating about numpy library

---
+++ number.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import numpy as np
+#creating 1D array
+a=np.array([2,3,5,79])
+b=np.array([12,13,15,179])
+
+#adding same 1D array
+c=a+b
+print(c)
+#multi
+m=a*b
+print(m)
+#adding particular value to whole array
+a5=a+5
+print(a5)
+#square of praticular array b
+b2=b**2
+print(b2)
